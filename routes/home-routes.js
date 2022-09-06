1a6d0a36e17630052ff34ccddcd7620818869c5e
Howard Wang <howard122696@gmail.com>
2022-09-06 01:51:09 -0700

    start reply ejs render

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 const Comment = require('../models/comment-model')
+const Reply = require('../models/reply-model')
 
 router.get('/', async (req, res) => {
     try{
       //show newest comment first 
       const commentList = await Comment.find().sort({date: -1})
-      res.render('home', { user: req.user, title: 'Home', commentList })
+      const replies = await Reply.find().sort({date: -1})
+      res.render('home', { user: req.user, title: 'Home', commentList, replies})
+      
   } catch(err){
       console.log(err)
   }
