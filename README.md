65a0e51789883d014949f5336422deaee5b62ca6
Howard Wang <howard122696@gmail.com>
2022-09-06 01:18:11 -0700

    readme and keysFormat

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+For keys, create a keys.js file in the config folder and input the info found in ./config/keyFormat.js. This was done in place of using .env
---
+++ config/keysFormat.js
@@ -0,0 +1,12 @@
+module.exports = {
+  google: {
+    clientID: 'google client id here',
+    clientSecret: 'google client secret here'
+  },
+  mongodb: {
+    dbURI: 'mongo connection string here'
+  },
+  session: {
+    cookieKey: 'somesecretkey'
+  }
+}
